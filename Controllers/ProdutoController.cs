7c1ca73d3d484b19f7936fbdb20a5e89d50c263a
Camadu <mariagferreira2312@gmail.com>
2025-05-15 16:27:14 -0300

    adicionando o banco e arrumando erros

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -4,31 +4,31 @@ using ProjetoEcommerce.Repositorio;
 
 namespace ProjetoEcommerce.Controllers
 {
-    public class ClienteController : Controller
+    public class ProdutoController : Controller
     {
         /* Declara uma variável privada somente leitura do tipo ClienteRepositorio chamada _clienteRepositorio.
          O "readonly" indica que o valor desta variável só pode ser atribuído no construtor da classe.
          ClienteRepositorio é uma classe do repositorio responsável por interagir com a camada de dados para gerenciar informações de usuários.*/
-        private readonly LoginRepositorio _clienteRepositorio;
+        private readonly ProdutoRepositorio _produtoRepositorio;
 
         /* Define o construtor da classe LoginController.
         Recebe uma instância de UsuarioRepositorio como parâmetro (injeção de dependência)*/
-        public ClienteController(LoginRepositorio clienteRepositorio)
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
         {
             /* O construtor é chamado quando uma nova instância de LoginController é criada.*/
-            _clienteRepositorio = clienteRepositorio;
+            _produtoRepositorio = produtoRepositorio;
         }
 
         public IActionResult Index()
         {
             /* Retorna a View padrão associada a esta Action,
              passando como modelo a lista de todos os clientes obtida do repositório.*/
-            return View(_clienteRepositorio.TodosClientes());
+            return View(_produtoRepositorio.TodosProdutos());
         }
 
 
         /* Action para exibir o formulário de cadastro de cliente (via Requisição GET)*/
-        public IActionResult CadastrarCliente()
+        public IActionResult CadastrarProduto()
         {
             //retorna a Página
             return View();
@@ -36,13 +36,13 @@ namespace ProjetoEcommerce.Controllers
 
         // Action que recebe e processa os dados que serão enviados pelo formulário de cadastro de cliente (via Requisição POST)
         [HttpPost]
-        public IActionResult CadastrarCliente(Cliente cliente)
+        public IActionResult CadastrarProduto(Produto produto)
         {
 
             /* O parâmetro 'cliente' recebe os dados enviados pelo formulário,
              que são automaticamente mapeados para as propriedades da classe Cliente.
              Chama o método no repositório para cadastrar o novo cliente no sistema.*/
-            _clienteRepositorio.Cadastrar(cliente);
+            _produtoRepositorio.Cadastrar(produto);
 
             //redireciona para pagina Index 'nameof(Index)' garante que o nome da Action seja usado corretamente,
             return RedirectToAction(nameof(Index));
@@ -50,20 +50,20 @@ namespace ProjetoEcommerce.Controllers
 
         /* Action para exibir o formulário de edição de um cliente específico (via Requisição GET)
          Este método recebe o 'id' do cliente a ser editado como parâmetro.*/
-        public IActionResult EditarCliente(int id)
+        public IActionResult EditarPorduto(int id)
         {
             // Obtém o cliente específico do repositório usando o ID fornecido.
-            var cliente = _clienteRepositorio.ObterCliente(id);
+            var produto = _produtoRepositorio.ObterProduto(id);
 
             // Verifica se o cliente foi encontrado. É uma boa prática tratar casos onde o ID é inválido.
-            if (cliente == null)
+            if (produto == null)
             {
                 // Você pode retornar um NotFound (código de status 404) ou outra resposta apropriada.
                 return NotFound();
             }
 
             // Retorna a View associada a esta Action (EditarCliente.cshtml),
-            return View(cliente);
+            return View(produto);
         }
 
 
@@ -74,10 +74,10 @@ namespace ProjetoEcommerce.Controllers
         /*[Bind] para especificar explicitamente quais propriedades do objeto Cliente podem ser vinculadas a partir dos dados do formulário.
         Isso é uma boa prática de segurança para evitar o overposting (onde um usuário malicioso pode enviar dados para propriedades
         que você não pretendia que fossem alteradas)*/
-        public IActionResult EditarCliente(int id, [Bind("CodCli, NomeCli,TelCli,EmailCli")] Cliente cliente)
+        public IActionResult EditarProduto(int id, [Bind("Id, Nome,Descricao,Preco, Quantidade")] Produto produto)
         {
             // Verifica se o ID fornecido na rota corresponde ao ID do cliente no modelo.
-            if (id != cliente.CodCli)
+            if (id != produto.Id)
             {
                 return BadRequest(); // Retorna um erro 400 se os IDs não corresponderem.
             }
@@ -87,10 +87,10 @@ namespace ProjetoEcommerce.Controllers
                 try
                 {
                     // Verifica se o cliente com o Codigo fornecido existe no repositório.
-                    if (_clienteRepositorio.Atualizar(cliente))
+                    if (_produtoRepositorio.Atualizar(produto))
                     {
                         //redireciona para a pagina index quando alterar
-                        return RedirectToAction(nameof(Index));                    
+                        return RedirectToAction(nameof(Index));
                     }
                 }
                 catch (Exception)
@@ -98,18 +98,18 @@ namespace ProjetoEcommerce.Controllers
                     // Adiciona um erro ao ModelState para exibir na View.
                     ModelState.AddModelError("", "Ocorreu um erro ao Editar.");
                     // Retorna a View com o modelo para exibir a mensagem de erro e os dados do formulário.
-                    return View(cliente);
+                    return View(produto);
                 }
             }
             // Se o ModelState não for válido, retorna a View com os erros de validação.
-            return View(cliente);
+            return View(produto);
         }
 
 
         public IActionResult ExcluirCliente(int id)
         {
             // Obtém o cliente específico do repositório usando o Codigo fornecido.
-            _clienteRepositorio.Excluir(id);
+            _produtoRepositorio.Excluir(id);
             // Retorna a View de confirmação de exclusão, passando o cliente como modelo.
             return RedirectToAction(nameof(Index));
         }
--- Repositorio/LoginRepositorio.cs
+++ Repositorio/LoginRepositorio.cs
@@ -7,174 +7,42 @@ using System.Data;
 
 namespace ProjetoEcommerce.Repositorio
 {
-       // Define a classe responsável por interagir com os dados de clientes no banco de dados
-        public class LoginRepositorio(IConfiguration configuration)
+    public class LoginRepositorio(IConfiguration configuration)
+    {
+
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        public Usuario ObterUsuario(string email)
         {
-            // Declara uma variável privada somente leitura para armazenar a string de conexão com o MySQL
-            private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
-
-
-            // Método para cadastrar um novo cliente no banco de dados
-            public void Cadastrar(Usuario usuario)
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
             {
-                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * from usuario where Id=@codigo ", conexao);
+                cmd.Parameters.AddWithValue("@email", MySqlDbType.VarChar).Value = email;
+
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+                Usuario usuario = new Usuario();
+
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                if (dr.Read())
                 {
-                    // Abre a conexão com o banco de dados MySQL
-                    conexao.Open();
-                    // Cria um novo comando SQL para inserir dados na tabela 'cliente'
-                    MySqlCommand cmd = new MySqlCommand("insert into usuario (Nome,Email,Senha) values (@nome, @email, @senha)", conexao); // @: PARAMETRO
-                                                                                                                                                 // Adiciona um parâmetro para o nome, definindo seu tipo e valor
-                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = usuario.Nome;
-                    // Adiciona um parâmetro para o telefone, definindo seu tipo e valor
-                    cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = usuario.Email;
-                    // Adiciona um parâmetro para o email, definindo seu tipo e valor
-                    cmd.Parameters.Add("@senha", MySqlDbType.VarChar).Value = usuario.Senha;
-                    // Executa o comando SQL de inserção e retorna o número de linhas afetadas
-                    cmd.ExecuteNonQuery();
-                    // Fecha explicitamente a conexão com o banco de dados (embora o 'using' já faça isso)
-                    conexao.Close();
-                }
-            }
-
-        // Método para Editar (atualizar) os dados de um cliente existente no banco de dados
-        public bool Atualizar(Usuario usuario)
-        {
-            try
-            {
-                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    // Abre a conexão com o banco de dados MySQL
-                    conexao.Open();
-                    // Cria um novo comando SQL para atualizar dados na tabela 'cliente' com base no código
-                    MySqlCommand cmd = new MySqlCommand("Update cliente set Nome=@nome, Email=@email, Senha=@senha " + " where Cod=@codigo ", conexao);
-                    // Adiciona um parâmetro para o código do cliente a ser atualizado, definindo seu tipo e valor
-                    cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = usuario.Id;
-                    // Adiciona um parâmetro para o novo nome, definindo seu tipo e valor
-                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = usuario.Nome;
-                    // Adiciona um parâmetro para o novo telefone, definindo seu tipo e valor
-                    cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = usuario.Email;
-                    // Adiciona um parâmetro para o novo email, definindo seu tipo e valor
-                    cmd.Parameters.Add("@senha", MySqlDbType.VarChar).Value = usuario.Senha;
-                    // Executa o comando SQL de atualização e retorna o número de linhas afetadas
-                    //executa e verifica se a alteração foi realizada
-                    int linhasAfetadas = cmd.ExecuteNonQuery();
-                    return linhasAfetadas > 0; // Retorna true se ao menos uma linha foi atualizada
-                   
-                }
-            }
-            catch (MySqlException ex)
-            {
-                // Logar a exceção (usar um framework de logging como NLog ou Serilog)
-                Console.WriteLine($"Erro ao atualizar usuario: {ex.Message}");
-                return false; // Retorna false em caso de erro
-               
-            }
-        }
-
-            // Método para listar todos os clientes do banco de dados
-            public IEnumerable<Usuario> TodosUsuario()
-            {
-                // Cria uma nova lista para armazenar os objetos Cliente
-                List<Usuario> Usuariolist = new List<Usuario>();
-
-                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    // Abre a conexão com o banco de dados MySQL
-                    conexao.Open();
-                    // Cria um novo comando SQL para selecionar todos os registros da tabela 'cliente'
-                    MySqlCommand cmd = new MySqlCommand("SELECT * from usuario", conexao);
-
-                    // Cria um adaptador de dados para preencher um DataTable com os resultados da consulta
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-                    // Cria um novo DataTable
-                    DataTable dt = new DataTable();
-                    // metodo fill- Preenche o DataTable com os dados retornados pela consulta
-                    da.Fill(dt);
-                    // Fecha explicitamente a conexão com o banco de dados 
-                    conexao.Close();
-
-                    // interage sobre cada linha (DataRow) do DataTable
-                    foreach (DataRow dr in dt.Rows)
+                    usuario = new Usuario
                     {
-                        // Cria um novo objeto Cliente e preenche suas propriedades com os valores da linha atual
-                        Usuariolist.Add(
-                                    new Usuario
-                                    {
-                                        Id = Convert.ToInt32(dr["id"]), // Converte o valor da coluna "codigo" para inteiro
-                                        Nome = ((string)dr["Nome"]), // Converte o valor da coluna "nome" para string
-                                        Email = ((string)dr["Email"]), // Converte o valor da coluna "telefone" para string
-                                        Senha = ((string)dr["Senha"]), // Converte o valor da coluna "email" para string
-                                    });
-                    }
-                    // Retorna a lista de todos os clientes
-                    return Usuariolist;
+                        Id = Convert.ToInt32(dr["Id"]),
+                        Nome = dr["Nome"].ToString(),
+                        Email = dr["Email"].ToString(),
+                        Senha = dr["Senha"].ToString(),
+                    };
+
+
                 }
+
+                return usuario;
             }
 
-            // Método para buscar um cliente específico pelo seu código (Codigo)
-            public Usuario ObterUsuario(int Codigo)
-            {
-                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    // Abre a conexão com o banco de dados MySQL
-                    conexao.Open();
-                    // Cria um novo comando SQL para selecionar um registro da tabela 'cliente' com base no código
-                    MySqlCommand cmd = new MySqlCommand("SELECT * from usuario where Id=@codigo ", conexao);
-
-                    // Adiciona um parâmetro para o código a ser buscado, definindo seu tipo e valor
-                    cmd.Parameters.AddWithValue("@codigo", Codigo);
-
-                    // Cria um adaptador de dados (não utilizado diretamente para ExecuteReader)
-                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
-
-                    // Declara um leitor de dados do MySQL
-                    MySqlDataReader dr;
-                // Cria um novo objeto Cliente para armazenar os resultados
-                 Usuario usuario = new Usuario();
-
-                    /* Executa o comando SQL e retorna um objeto MySqlDataReader para ler os resultados
-                    CommandBehavior.CloseConnection garante que a conexão seja fechada quando o DataReader for fechado*/
-
-                    dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-                    // Lê os resultados linha por linha
-                    while (dr.Read())
-                    {
-                        // Preenche as propriedades do objeto Cliente com os valores da linha atual
-                        usuario.Id = Convert.ToInt32(dr["Id"]);//propriedade Codigo e convertendo para int
-                        usuario.Nome = (string)(dr["Nome"]); // propriedade Nome e passando string
-                        usuario.Email = (string)(dr["Email"]); //propriedade telefone e passando string
-                        usuario.Senha = (string)(dr["Senha"]); //propriedade email e passando string
-                    }
-                    // Retorna o objeto Cliente encontrado (ou um objeto com valores padrão se não encontrado)
-                    return usuario;
-                }
-            }
-
-
-            // Método para excluir um cliente do banco de dados pelo seu código (ID)
-            public void Excluir(int Id)
-            {
-                // Bloco using para garantir que a conexão seja fechada e os recursos liberados após o uso
-                using (var conexao = new MySqlConnection(_conexaoMySQL))
-                {
-                    // Abre a conexão com o banco de dados MySQL
-                    conexao.Open();
-
-                    // Cria um novo comando SQL para deletar um registro da tabela 'cliente' com base no código
-                    MySqlCommand cmd = new MySqlCommand("delete from usuario where Id=@codigo", conexao);
-
-                    // Adiciona um parâmetro para o código a ser excluído, definindo seu tipo e valor
-                    cmd.Parameters.AddWithValue("@codigo", Id);
-
-                    // Executa o comando SQL de exclusão e retorna o número de linhas afetadas
-                    int i = cmd.ExecuteNonQuery();
-
-                    conexao.Close(); // Fecha  a conexão com o banco de dados
-                }
-            }
         }
+    }
 }
